43d5f2ea7eb4be41a07f6024eda0e306eab5f301
zsdf21 <zsdf21@gmail.com>
2020-12-08 09:39:13 +0900

    pem update

--- run.py
+++ run.py
@@ -1,11 +1,13 @@
 # 간단한 기본 서버 구축
-from flask import flask
+from flask import Flask
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return 'aws 홈페이지'
+
 
 # run.py가 엔트리포인트일 경우에만 작동
 # 리눅스 서버로 가면 wsgi.py이 엔트리포인트이므로 작동 안함
